c0c292daffd3ed86a9e98f124cdb4aa0da8e0453
Raju <r4ju.riy4nd4@gmail.com>
2020-05-02 22:38:50 +0700

    add base services and pod lib

---
+++ suitmedia/suitmedia/Common/Helper/AppConstants.swift
@@ -0,0 +1,18 @@
+//
+//  AppConstants.swift
+//  suitmedia
+//
+//  Created by Jelajah Data Semesta on 02/05/20.
+//  Copyright © 2020 Raju Riyanda. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants {
+//Main Endpoints
+static let baseUrl       : String = "https://reqres.in/api/"
+}
+
+enum Api:String {
+case user = "users"
+}
---
+++ suitmedia/suitmedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  suitmedia
+//
+//  Created by Jelajah Data Semesta on 02/05/20.
+//  Copyright © 2020 Raju Riyanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
