1e94cf0fced792976eddfcd2ea6d57413eb80708
Raju <r4ju.riy4nd4@gmail.com>
2020-05-02 23:33:20 +0700

    Add image and base view

--- suitmedia/suitmedia/AppDelegate.swift
+++ suitmedia/suitmedia/AppDelegate.swift
@@ -11,7 +11,8 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    static let current = UIApplication.shared.delegate as! AppDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -23,6 +24,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    /**
+     This function set the view controller become a root of application
+     **/
+    func setRootViewController(viewController: UIViewController) {
+        
+        UIView.transition(with: window!, duration: 0.25, options: [.transitionCrossDissolve, .allowAnimatedContent], animations: {
+            
+            let oldState = UIView.areAnimationsEnabled
+            UIView.setAnimationsEnabled(false)
+            self.window?.rootViewController = viewController
+            UIView.setAnimationsEnabled(oldState)
+            
+        }) { (finished) in
+            
+        }
     }
 
 
